6717dff0b48d101e0b36db828db797f2ea81182c
Shobhit Khandelwal <shobhit.khandelwal@scrumdigital.com>
2022-05-26 11:14:04 +0530

    Changes

--- app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Admin/Auth/AuthenticatedSessionController.php
@@ -31,7 +31,7 @@ class AuthenticatedSessionController extends Controller
     {
         $request->authenticate();
         $request->session()->regenerate();
-        return redirect()->intended(RouteServiceProvider::ADMIN);
+        return redirect(RouteServiceProvider::ADMIN);
     }
 
     /**
