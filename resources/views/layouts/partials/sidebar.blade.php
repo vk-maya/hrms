810da1f95eee2d7842889db3c978b5268727fb86
vikas-sd <vikas.jakhar@scrumdigital.com>
2022-05-30 12:55:20 +0530

    validate

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -34,8 +34,7 @@
                             <li><a class="@if(\Request::route()->getName() == 'employees.daily.task') active @endif" href="{{route('employees.daily.task')}}">Daily Task</a></li>
                         @else
                             <li><a disabled>Daily Task</a></li>
-                        @endif
-                    
+                        @endif                    
                         <li><a class="@if(\Request::route()->getName() == 'employees.show-list') active @endif" href="{{route('employees.show-list')}}">List Task</a></li>
                     </ul>
                 </li>
