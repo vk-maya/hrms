52aca580544b6371325660bf1f3b2f29bfec6aab
vikas-sd <vikas.jakhar@scrumdigital.com>
2022-06-09 17:58:35 +0530

    working hour Show

--- resources/views/admin/attendance/attendance.blade.php
+++ resources/views/admin/attendance/attendance.blade.php
@@ -93,7 +93,8 @@
                                         @endphp
                                         @if (!empty($attend))
                                             @if ($attend->attendance == 'P')
-                                                <td><i class="fa fa-check text-success"></i></td>
+                                                <td><a href="javascript:void(0);" data-bs-toggle="modal"
+                                                    data-bs-target="#attendance_info"><i class="fa fa-check text-success"></i></td>
                                             @else
                                                 <td><i class="fa fa-close text-danger"></i> </td>
                                             @endif
@@ -105,6 +106,60 @@
                             @endforeach                                             
                         </tbody>
                     </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal custom-modal fade" id="attendance_info" role="dialog">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Attendance Info</h5>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col">
+                        <div class="card punch-status">
+                            <div class="card-body">
+                                <h5 class="card-title">Timesheet <small class="text-muted">11 Mar
+                                        2019</small></h5>
+                                <div class="punch-det">
+                                    <h6>Punch In at</h6>
+                                    <p>Wed, 11th Mar 2019 10.00 AM</p>
+                                </div>
+                                <div class="punch-info">
+                                    <div class="punch-hours">
+                                        <span>3.45 hrs</span>
+                                    </div>
+                                </div>
+                                <div class="punch-det">
+                                    <h6>Punch Out at</h6>
+                                    <p>Wed, 20th Feb 2019 9.00 PM</p>
+                                </div>
+                                <div class="statistics">
+                                    <div class="row">
+                                        <div class="col-md-6 col-6 text-center">
+                                            <div class="stats-box">
+                                                <p>Break</p>
+                                                <h6>1.21 hrs</h6>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 col-6 text-center">
+                                            <div class="stats-box">
+                                                <p>Overtime</p>
+                                                <h6>3 hrs</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                 
                 </div>
             </div>
         </div>
