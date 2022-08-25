b2a855aec34d41dcca1b35bdf4588698c1cb2299
Shobhit Khandelwal <shobhit.khandelwal@scrumdigital.com>
2022-08-25 15:53:46 +0530

    Monthly Update

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -10,15 +10,15 @@ class Attendance extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'in_time', 'out_time', 'work_time', 'date', 'day', 'month', 'year', 'attendance', 'status', 'passdate'];
+    protected $fillable = ['user_id', 'in_time', 'out_time', 'work_time', 'date', 'day', 'month', 'year', 'attendance', 'status', 'passdate', 'mark'];
 
     public function userinfo(){
         return $this->hasOne(User::class);
     }
     public function wfh(){
-        return $this->hasOne(WorkFromHome::class,'user_id','user_id');    
+        return $this->hasOne(WorkFromHome::class,'user_id','user_id');
     }
-  
+
     public function leavestatus(){
         return $this->hasOne(Leave::class,'user_id','user_id');
     }
