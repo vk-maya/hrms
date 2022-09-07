44f71e08a845a235d156316f6adef313226faf9d
Shobhit Khandelwal <shobhit.khandelwal@scrumdigital.com>
2022-09-07 11:08:17 +0530

    Changes

--- resources/views/admin/attendance/attendance.blade.php
+++ resources/views/admin/attendance/attendance.blade.php
@@ -105,61 +105,6 @@
                     </div>
                 </div>
             </form>
-            <div class="row">
-                <div class="col-sm-12">
-                    <h3 class="page-title">Attendance</h3>
-                    <ul class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Dashboard</a></li>
-                        <li class="breadcrumb-item active">Attendance</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="row filter-row justify-content-center">
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="form-group form-focus">
-                    <input type="text" class="form-control floating">
-                    <label class="focus-label">Employee Name</label>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="form-group form-focus select-focus">
-                    <select class="select floating">
-                        <option>-</option>
-                        <option>Jan</option>
-                        <option>Feb</option>
-                        <option>Mar</option>
-                        <option>Apr</option>
-                        <option>May</option>
-                        <option>Jun</option>
-                        <option>Jul</option>
-                        <option>Aug</option>
-                        <option>Sep</option>
-                        <option>Oct</option>
-                        <option>Nov</option>
-                        <option>Dec</option>
-                    </select>
-                    <label class="focus-label">Select Month</label>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="form-group form-focus select-focus">
-                    <select class="select floating ">
-                        <option>-</option>
-                        <option>2019</option>
-                        <option>2018</option>
-                        <option>2017</option>
-                        <option>2016</option>
-                        <option>2015</option>
-                    </select>
-                    <label class="focus-label">Select Year</label>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="search-btn">
-                    <a href="#" class="btn btn-success"> Search </a>
-                </div>
-            </div>
         </div>
         <div class="row">
             <div class="col-lg-12">
